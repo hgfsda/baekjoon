eb29934c96fa837ee07a5df070172ed3feab8049
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-21 16:18:46 +0900

    18108

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 int main() {
-	char str[51];
-
-	cin >> str;
-	cout << str << "??!";
+	int arr;
+	cin >> arr;
+	if (arr < 1000 || arr > 3000)
+		return 1;
+	cout << arr - 543 << endl;
 	
 	return 0;
 }
