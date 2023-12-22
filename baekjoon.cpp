e407b184aa89f02c994c3d041dd1f7ba5c043dea
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-22 11:20:13 +0900

    2438

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -5,12 +5,14 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int T, A, B;
-	cin >> T;
-	for (int i = 0; i < T; ++i) {
-		cin >> A >> B;
-		cout << "Case #" << i+1 << ": " << A << " + " << B <<
-			" = " << A + B << '\n';
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j <= i; ++j) {
+			cout << "*";
+		}
+		cout << '\n';
 	}
+
 	return 0;
 }
