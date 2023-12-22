62587c0dd3c1e8e151415bade5e829107e94a30b
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-22 09:44:17 +0900

    10172

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main() {
 
-	cout << "\\    /\\" << endl;
-	cout << " )  ( ')" << endl;
-	cout << "(  /  )" << endl;
-	cout << " \\(__)|" << endl;
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
 
 	return 0;
 }
