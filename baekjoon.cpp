2c1e6a73273d3dd5787401bab84dfedefc44fef5
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-22 09:39:33 +0900

    10171

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-	long long A, B, C;
 
-	cin >> A >> B >> C;
-
-	cout << A + B + C << endl;
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
 
 	return 0;
 }
