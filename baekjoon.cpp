f9f06651a15c08adb80e17cb6f68fa7246507748
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-22 11:26:14 +0900

    10952

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int N;
-	cin >> N;
-	for (int i = 0; i < N; ++i) {
-		for (int j = N-1; j >= 0; --j) {
-			if (j > i)
-				cout << " ";
-			else
-				cout << "*";
-		}
-		cout << '\n';
+int main() {;
+	int A, B;
+	cin >> A >> B;
+	while (A != 0 && B != 0) {
+		cout << A + B << '\n';
+		cin >> A >> B;
 	}
 	return 0;
 }
