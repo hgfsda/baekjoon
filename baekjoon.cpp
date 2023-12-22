b00aa6894f05c8f2c2e85e2c9429a3e13f6ee77d
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-22 13:33:10 +0900

    10813

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 
 int main() {
-	int A[100], N, M, i, j, k;
+	int A[100], N, M, i, j, temp;
 	cin >> N >> M;
 	for (int s = 0; s < N; ++s)
-		A[s] = 0;
+		A[s] = s+1;
 	for (int s = 0; s < M; ++s) {
-		cin >> i >> j >> k;
-		for (int t = i - 1; t < j; ++t)
-			A[t] = k;
+		cin >> i >> j;
+		temp = A[i - 1];
+		A[i - 1] = A[j - 1];
+		A[j - 1] = temp;
 	}
 	for (int s = 0; s < N; ++s)
 		cout << A[s] << ' ';
