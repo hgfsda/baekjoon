4cca97adb2ce727c8738b2f1528f23fa98629e27
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-22 14:34:19 +0900

    27866

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -2,16 +2,9 @@
 using namespace std;
 
 int main() {
-	int N, score[1000], max = 0;
-	float total = 0;
-	cin >> N;
-	for (int i = 0; i < N; ++i) {
-		cin >> score[i];
-		if (score[i] > max) max = score[i];
-	}
-	for (int i = 0; i < N; ++i) {
-		total += score[i] / static_cast<float>(max) * 100;
-	}
-	cout << total / N;
+	int num;
+	char S[1001];
+	cin >> S >> num;
+	cout << S[num - 1];
 	return 0;
 }
