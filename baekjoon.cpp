3e57dd1de0e46900673160a320f8e553919439e0
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-22 14:07:42 +0900

    3052

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 
 int main() {
-	int N[30], cnt;
-	for (int i = 0; i < 30; ++i) {
-		N[i] = 0;
+	int N[10], total[42], cnt = 0;
+	for (int i = 0; i < 42; ++i) {
+		total[i] = 0;
 	}
-	for (int i = 0; i < 28; ++i) {
-		cin >> cnt;
-		N[cnt - 1] = 1;
+	for (int i = 0; i < 10; ++i) {
+		cin >> N[i];
+		N[i] = N[i] % 42;
+		total[N[i]] += 1;
 	}
-	for (int i = 0; i < 30; ++i)
-		if (N[i] == 0)
-			cout << i + 1 << '\n';
+	for (int i = 0; i < 42; ++i) {
+		if (total[i] != 0)
+			++cnt;
+	}
+	cout << cnt;
 	return 0;
 }
