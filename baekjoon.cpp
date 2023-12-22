17c32aacf704ade11c5d17f0e322742dcca10764
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-22 10:52:53 +0900

    25304

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 int main() {
-
-	int n, total = 0;
-	cin >> n;
-
-	for (int i = 1; i <= n; ++i)
-		total += i;
-
-	cout << total << endl;
+	int X, N, a[100], b[100], total = 0;
+	cin >> X >> N;
+	for (int i = 0; i < N; ++i) {
+		cin >> a[i] >> b[i];
+		total += a[i] * b[i];
+	}
+	if (total == X)
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
 
 	return 0;
 }
