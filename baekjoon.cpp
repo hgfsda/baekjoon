c4192ff77a9e210ef128f6671a6cece1ba9128e5
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-22 10:43:51 +0900

    10950

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 int main() {
 
-	int A;
-	cin >> A;
+	int A[10000], B[10000], T;
+	cin >> T;
 	
-	for (int i = 1; i < 10; ++i) {
-		cout << A << " * " << i << " = " << A*i << endl;
+	for (int i = 0; i < T; ++i) {
+		cin >> A[i] >> B[i];
 	}
+	for (int i = 0; i < T; ++i)
+		cout << A[i] + B[i] << endl;
 
 	return 0;
 }
