729546fc1d81ca491e767103935a33587ce2324e
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-22 11:15:58 +0900

    11022

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -9,7 +9,8 @@ int main() {
 	cin >> T;
 	for (int i = 0; i < T; ++i) {
 		cin >> A >> B;
-		cout << "Case #" << i+1 << ": " << A + B << '\n';
+		cout << "Case #" << i+1 << ": " << A << " + " << B <<
+			" = " << A + B << '\n';
 	}
 	return 0;
 }
