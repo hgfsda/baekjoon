25eb79b4586c59bf3a0b56529576832f08daeb6b
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-22 10:35:32 +0900

    2739

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -3,25 +3,12 @@ using namespace std;
 
 int main() {
 
-	int A, B, C;
-	cin >> A >> B >> C;
+	int A;
+	cin >> A;
 	
-	if (A == B && A == C && B == C)
-		cout << 10000 + A * 1000 << endl;
-	else if (A != B && A != C && B != C) {
-		if (A >= B && A >= C)
-			cout << A * 100 << endl;
-		else if (B >= A && B >= C)
-			cout << B * 100 << endl;
-		else if (C >= B && C >= A)
-			cout << C * 100 << endl;
+	for (int i = 1; i < 10; ++i) {
+		cout << A << " * " << i << " = " << A*i << endl;
 	}
-	else if (A == B && A != C)
-		cout << 1000 + A * 100 << endl;
-	else if (A == C && A != B)
-		cout << 1000 + A * 100 << endl;
-	else if (C == B && B != A)
-		cout << 1000 + B * 100 << endl;
 
 	return 0;
 }
