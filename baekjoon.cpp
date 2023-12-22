9a6aff48242fa8b31aa5326d00fb29b91097b473
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-22 14:48:30 +0900

    11654

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -3,15 +3,10 @@
 using namespace std;
 
 int main() {
-	int T;
-	char S[10][1000], s[10], e[10];
-	cin >> T;
-	for (int i = 0; i < T; ++i) {
-		cin >> S[i];
-		s[i] = S[i][0];
-		e[i] = S[i][strlen(S[i]) - 1];
-	}
-	for (int i = 0; i < T; ++i)
-		cout << s[i] << e[i] << '\n';
+	char A;
+	cin >> A;
+	int change_A = A;
+	cout << change_A;
+	
 	return 0;
 }
