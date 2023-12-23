b6e6ec5652d651cb1ed396c68dc6257aa5d8c363
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-23 18:47:39 +0900

    1152

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -6,18 +6,14 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int T, R;
-	string str, result[1001];
-	cin >> T;
-	for (int i = 0; i < T; ++i) {
-		int cnt = -1;
-		cin >> R >> str;
-		for (int j = 0; j < R * str.length(); ++j) {
-			if (j % R == 0)
-				++cnt;
-			result[i] += str[cnt];
-		}
-		cout << result[i] << '\n';
+	string str;
+	int cnt = 1;
+	getline(cin, str);
+	for (int i = 0; i < str.length(); ++i) {
+		if (str[i] == ' ' && i != str.length() - 1)
+			++cnt;
 	}
+	if (str[0] == ' ') --cnt;
+	cout << cnt;
 	return 0;
 }
