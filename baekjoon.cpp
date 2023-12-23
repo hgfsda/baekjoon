051ee0da4133dfbc4643d287fbea84f9c09911f6
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-23 17:55:40 +0900

    10809

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int N, total = 0;
-	char arr[101];
-	cin >> N;
-	cin >> arr;
-	for (int i = 0; i < N; ++i) {
-		total += arr[i] - '0';
-	}
-	cout << total;
+	string str;
+	int alphabet[26];
+	cin >> str;
+	for (int i = 0; i < 26; ++i)
+		alphabet[i] = -1;
+	for (int i = 0; i < str.length(); ++i)
+		if (alphabet[str[i] - 'a'] == -1) alphabet[str[i] - 'a'] = i;
+	for (int i = 0; i < 26; ++i)
+		cout << alphabet[i] << ' ';
 	return 0;
 }
